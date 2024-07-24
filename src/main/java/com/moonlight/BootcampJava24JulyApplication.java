0d03aa5c46374c54410d2350ddb49da0ff86adfd
Oreshcode <Oreshcode@gmail.com>
2024-07-24 20:21:43 +0300

    Init commit with pom/yml configured

---
+++ src/main/java/com/moonlight/BootcampJava24JulyApplication.java
@@ -0,0 +1,13 @@
+package com.moonlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampJava24JulyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampJava24JulyApplication.class, args);
+	}
+
+}
