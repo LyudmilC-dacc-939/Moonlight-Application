03c1621d99d99d656a33f913f0efe0dd43577851
LAC_Knightmare <Whats_with_lusk0@abv.bg>
2024-07-26 20:48:04 +0300

    .gitignore small change and user code refinement

--- src/main/java/com/moonlight/model/User.java
+++ src/main/java/com/moonlight/model/User.java
@@ -2,6 +2,7 @@ package com.moonlight.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,14 +18,19 @@ public class User {
     @Column(unique = true)
     private Long id;
     @Column(name = "FIRST_NAME", nullable = false)
+    @Size(min = 2, max = 255, message = "First name must be between {min} and {max} characters")
     private String firstName;
     @Column(name = "LAST_NAME", nullable = false)
+    @Size(min = 2, max = 255, message = "Last name must be between {min} and {max} characters")
     private String lastName;
-    @Column(name = "EMAIL",unique = true ,nullable = false)
+    @Column(name = "EMAIL", unique = true ,nullable = false)
+    @Size(min = 5, max = 255, message = "Email address must be between {min} and {max} characters")
     private String emailAddress;
     @Column(name = "PHONE_NUMBER", nullable = false)
+    @Size(max = 15, message = "Phone number length must be at most {max} characters and must start with '+' symbol")
     private String phoneNumber;
     @Column(name = "PASSWORD", nullable = false)
+    @Size(min = 8, max = 30,message = "User's password must be between {min} and {max} symbols")
     private String password;
     private Instant dateCreated;
 
