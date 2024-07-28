1a93e988dbb8da987e3121e3aaff9bda724c6ffa
MartinZhelyazkov <marto123_123_1990@abv.bg>
2024-07-29 02:56:22 +0300

    Created Repository for User, changed java version to 20, optimized imports

--- src/main/java/com/moonlight/model/User.java
+++ src/main/java/com/moonlight/model/User.java
@@ -23,14 +23,14 @@ public class User {
     @Column(name = "LAST_NAME", nullable = false)
     @Size(min = 2, max = 255, message = "Last name must be between {min} and {max} characters")
     private String lastName;
-    @Column(name = "EMAIL", unique = true ,nullable = false)
+    @Column(name = "EMAIL", unique = true, nullable = false)
     @Size(min = 5, max = 255, message = "Email address must be between {min} and {max} characters")
     private String emailAddress;
     @Column(name = "PHONE_NUMBER", nullable = false)
     @Size(max = 15, message = "Phone number length must be at most {max} characters and must start with '+' symbol")
     private String phoneNumber;
     @Column(name = "PASSWORD", nullable = false)
-    @Size(min = 8, max = 30,message = "User's password must be between {min} and {max} symbols")
+    @Size(min = 8, max = 30, message = "User's password must be between {min} and {max} symbols")
     private String password;
     private Instant dateCreated;
 
--- src/main/java/com/moonlight/model/UserRole.java
+++ src/main/java/com/moonlight/model/UserRole.java
@@ -1,11 +1,8 @@
 package com.moonlight.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Set;
 
 @Entity(name = "ROLES")
 @Data
