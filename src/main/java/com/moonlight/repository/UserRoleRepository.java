ba42a4bc2e68a7a98195f2263e8e288e8fa93fce
Lyudmil <lyudmil_filipov@abv.bg>
2024-07-29 19:56:22 +0300

    added a check to see if the admin role exists in the database, as well as an admin-details.txt file containing an example admin profile

--- src/main/java/com/moonlight/repository/UserRoleRepository.java
+++ src/main/java/com/moonlight/repository/UserRoleRepository.java
@@ -4,8 +4,10 @@ import com.moonlight.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole,Long> {
 
-    UserRole findByUserRole(String userRole);
+    Optional<UserRole> findByUserRole(String userRole);
 }
