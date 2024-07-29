409d9da5829041626d7e07e97553c1caece345eb
Lyudmil <lyudmil_filipov@abv.bg>
2024-07-29 17:45:29 +0300

    Added Admin Asset, UserRoleRepository, UserAlreadyExistsException and GlobalExceptionHandler

--- src/main/java/com/moonlight/repository/UserRoleRepository.java
+++ src/main/java/com/moonlight/repository/UserRoleRepository.java
@@ -4,10 +4,8 @@ import com.moonlight.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole,Long> {
 
-    Optional<UserRole> findByUserRole(String userRole);
+    UserRole findByUserRole(String userRole);
 }
