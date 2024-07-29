f6f723e592ed811d3056c3be64398229ed641954
Lyudmil <lyudmil_filipov@abv.bg>
2024-07-29 17:45:29 +0300

    Added Admin Asset, UserRoleRepository, UserAlreadyExistsException and GlobalExceptionHandler

--- src/main/java/com/moonlight/asset/AdminAsset.java
+++ src/main/java/com/moonlight/asset/AdminAsset.java
@@ -1,6 +1,6 @@
 package com.moonlight.asset;
 
-import com.moonlight.advice.exception.UserAlreadyExistsException;
+import com.moonlight.exception.UserAlreadyExistsException;
 import com.moonlight.model.User;
 import com.moonlight.model.UserRole;
 import com.moonlight.repository.UserRepository;
@@ -23,19 +23,12 @@ public class AdminAsset implements CommandLineRunner {
     private UserRoleRepository userRoleRepository;
     @Override
     public void run(String... args) throws Exception {
-        Optional<UserRole> userRole = userRoleRepository.findByUserRole("ROLE_ADMIN");
-        if (userRole.isEmpty()) {
-            UserRole roleAdmin = new UserRole();
-            roleAdmin.setUserRole("ROLE_ADMIN");
-            userRoleRepository.save(roleAdmin);
-        } else {
-            userRole.get();
-        }
+        UserRole userRole = userRoleRepository.findByUserRole("ROLE_ADMIN");
         Optional<User> user = userRepository.findByUserRole(userRole);
         if (user.isPresent()) {
             throw new UserAlreadyExistsException(String.format("User with role admin already exists"));
         }   else {
-            String filePath = "src/main/resources/admin-details.txt";
+            String filePath = "Text file path here";
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
             String line;
             while ((line = reader.readLine()) != null) {
@@ -48,7 +41,7 @@ public class AdminAsset implements CommandLineRunner {
                 newAdmin.setPhoneNumber(data[3].trim());
                 newAdmin.setPassword(data[4].trim());
                 newAdmin.setDateCreated(Instant.now());
-                newAdmin.setUserRole(userRole.get());
+                newAdmin.setUserRole(userRole);
 
                 userRepository.save(newAdmin);
             }
