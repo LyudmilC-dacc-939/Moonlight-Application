494f071a5f19f07932c385bcd72807f76b09d14e
Oreshcode <Oreshcode@gmail.com>
2024-07-29 20:34:06 +0300

    Service Layer, Small Edit to application.yml

---
+++ src/main/java/com/moonlight/service/UserService.java
@@ -0,0 +1,26 @@
+package com.moonlight.service;
+
+import com.moonlight.dto.UserRequest;
+import com.moonlight.model.User;
+import io.swagger.v3.oas.annotations.Operation;
+
+import java.util.Optional;
+
+public interface UserService {
+    @Operation(summary = "Register a new user", description = "Creates a new user in the database.")
+    User registerUser(UserRequest userRequest);
+
+    @Operation(summary = "Get user by ID", description = "Retrieves an user by their ID.")
+    Optional<User> getUserById(Long id);
+
+    // Email is our "username" to be used when retrieving user information.
+    @Operation(summary = "Retrieve a user by email", description = "Returns a User account information found by their email address.")
+    Optional<User> getUserByEmail(String email);
+
+    // Delete User is not required, if you decide to remove it, delete this and it's method in the Impl file
+    @Operation(summary = "Deletes an User by ID", description = "Deletes an user from the database by their ID")
+    void deleteUser(Long id);
+
+    @Operation(summary = "Logs in an Existing User", description = "Login an user using their email and password")
+    User login(String email, String password);
+}
---
+++ src/main/java/com/moonlight/service/impl/UserServiceImpl.java
@@ -0,0 +1,68 @@
+package com.moonlight.service.impl;
+
+import com.moonlight.advice.exception.RecordNotFoundException;
+import com.moonlight.dto.UserRequest;
+import com.moonlight.model.User;
+import com.moonlight.model.UserRole;
+import com.moonlight.repository.UserRepository;
+import com.moonlight.service.UserService;
+import jakarta.validation.ConstraintViolationException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public User registerUser(UserRequest userRequest) {
+        if (userRepository.findByEmail(userRequest.getEmail()).isPresent()) {
+            throw new ConstraintViolationException("Email is already taken", null);
+        }
+
+        User user = new User();
+        UserRole role = new UserRole();
+        // Temporary setting the regular use to "ROLE_CLIENT", it's not decided what it will be called yet, and we have not
+        // created the ROLES table/id/names yet - TBD
+        role.setUserRole("ROLE_CLIENT");
+        user.setEmailAddress(userRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+        user.setDateCreated(Instant.now());
+        user.setUserRole(role);
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException("User not found"));
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User login(String email, String password) {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RecordNotFoundException("User not found"));
+        if (passwordEncoder.matches(password, user.getPassword())) {
+            return user;
+        } else {
+            throw new IllegalArgumentException("Invalid username or password");
+        }
+    }
+}
