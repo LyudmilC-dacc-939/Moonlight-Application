0236318783ac839658806664a1c36b3d0c228e38
Oreshcode <Oreshcode@gmail.com>
2024-07-31 18:47:10 +0300

    Admin Asset bug fix

--- src/main/java/com/moonlight/asset/AdminAsset.java
+++ src/main/java/com/moonlight/asset/AdminAsset.java
@@ -38,7 +38,7 @@ public class AdminAsset implements CommandLineRunner {
 
         // Check if a user with the admin role already exists
         if (userRepository.findByUserRole(userRole.get()).isPresent()) {
-            throw new UserAlreadyExistsException("User with role admin already exists");
+            System.out.println("User with role admin already exists");
         }
 
         // Read admin details from the file
--- src/main/java/com/moonlight/service/impl/UserServiceImpl.java
+++ src/main/java/com/moonlight/service/impl/UserServiceImpl.java
@@ -7,16 +7,14 @@ import com.moonlight.model.UserRole;
 import com.moonlight.repository.UserRepository;
 import com.moonlight.service.UserService;
 import jakarta.validation.ConstraintViolationException;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.util.Optional;
 
 @Service
-@Data
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
