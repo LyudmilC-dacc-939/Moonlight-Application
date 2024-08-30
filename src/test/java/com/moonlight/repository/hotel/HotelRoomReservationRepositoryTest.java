cc38306055ec3692b87fa6267fc1338f6442b78f
LAC_Knightmare <Whats_with_lusk0@abv.bg>
2024-08-30 03:07:59 +0300

    added functionality to tests for HotelRoomReservationRepository

--- src/test/java/com/moonlight/repository/hotel/HotelRoomReservationRepositoryTest.java
+++ src/test/java/com/moonlight/repository/hotel/HotelRoomReservationRepositoryTest.java
@@ -2,6 +2,7 @@ package com.moonlight.repository.hotel;
 
 import com.moonlight.model.enums.RoomType;
 import com.moonlight.model.enums.RoomView;
+import com.moonlight.model.hotel.Amenity;
 import com.moonlight.model.hotel.HotelRoom;
 import com.moonlight.model.hotel.HotelRoomReservation;
 import com.moonlight.model.user.User;
@@ -10,14 +11,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import javax.mail.search.SearchTerm;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class HotelRoomReservationRepositoryTest {
 
     @Autowired
@@ -29,6 +34,9 @@ class HotelRoomReservationRepositoryTest {
     @Autowired
     private HotelRoomRepository hotelRoomRepository;
 
+    @Autowired
+    private AmenityRepository amenityRepository;
+
     private User user;
     private HotelRoom hotelRoom;
 
@@ -38,13 +46,31 @@ class HotelRoomReservationRepositoryTest {
         user = new User();
         user.setFirstName("Test ");
         user.setLastName("User");
-        user = userRepository.save(user);
+        user.setEmailAddress("testov.email@abv.bg");
+        user.setPassword("pAr0la_za_T3s7");
+        user.setPhoneNumber("008553285493");
+        userRepository.save(user);
 
         hotelRoom = new HotelRoom();
         hotelRoom.setRoomNumber(105L);
         hotelRoom.setRoomType(RoomType.STANDARD);
         hotelRoom.setRoomView(RoomView.GARDEN);
+
+        Amenity amenity = new Amenity();
+        amenity.setAmenity("Couch");
+        amenityRepository.save(amenity);
+        Amenity amenity2 = new Amenity();
+        amenity2.setAmenity("Jacuzzi");
+        amenityRepository.save(amenity2);
+
+        Set<Amenity> amenities = Set.of(amenity, amenity2);
+        hotelRoom.setAmenities(amenities);
+        Set<HotelRoom> hotelRooms = Set.of(hotelRoom);
+        amenity.setHotelRooms(hotelRooms);
+        amenity2.setHotelRooms(hotelRooms);
         hotelRoom = hotelRoomRepository.save(hotelRoom);
+
+
     }
 
     @Test
@@ -60,11 +86,17 @@ class HotelRoomReservationRepositoryTest {
     @Test
     public void testFindByUserIdOrderByStartDate() {
         HotelRoomReservation reservation1 = createReservation();
+        reservation1.setUser(user);
+        reservation1.setHotelRoom(hotelRoom);
         reservation1.setStartDate(LocalDate.of(2023, 8, 1));
+        reservation1.setEndDate(LocalDate.of(2023, 8, 14));
         hotelRoomReservationRepository.save(reservation1);
 
         HotelRoomReservation reservation2 = createReservation();
+        reservation2.setUser(user);
+        reservation2.setHotelRoom(hotelRoom);
         reservation2.setStartDate(LocalDate.of(2023, 7, 1));
+        reservation2.setEndDate(LocalDate.of(2023, 7, 10));
         hotelRoomReservationRepository.save(reservation2);
 
         List<HotelRoomReservation> reservations = hotelRoomReservationRepository.findByUserIdOrderByStartDate(user.getId());
@@ -75,12 +107,16 @@ class HotelRoomReservationRepositoryTest {
 
     @Test
     public void testFindByUserIdOrderByStartDate_NullUserId() {
-        HotelRoomReservation reservation = createReservation();
-        hotelRoomReservationRepository.save(reservation);
+        HotelRoomReservation reservation1 = createReservation();
+        reservation1.setUser(user);
+        reservation1.setHotelRoom(hotelRoom);
+        reservation1.setStartDate(LocalDate.of(2023, 8, 1));
+        reservation1.setEndDate(LocalDate.of(2023, 8, 14));
+        hotelRoomReservationRepository.save(reservation1);
 
         List<HotelRoomReservation> reservations = hotelRoomReservationRepository.findByUserIdOrderByStartDate(null);
         assertThat(reservations).hasSize(1);
-        assertThat(reservations.get(0)).isEqualTo(reservation);
+        assertThat(reservations.get(0)).isEqualTo(reservation1);
     }
 
     @Test
